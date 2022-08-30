f0e6a5bb098d18168f788175c03f08e9984370c7
munozvicente <vanm@outlook.cl>
2022-08-30 18:40:20 -0300

    Added permission classes for views, new User Serializer with token

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,45 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Product
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    lastname = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'isAdmin', 'username', 'email', 'name', 'lastname']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+    def get_lastname(self, obj):
+        lastname = obj.last_name
+        return lastname
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'isAdmin', 'username', 'email', 'name', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.getRoutes, name=""),
+    path('users/', views.getUsers, name="users"),
+    path('users/profile/', views.getUserProfile, name="users-profile"),
     path('products/', views.getProducts, name="products"),
-    path('products/<str:pk>', views.getProduct, name="product")
+    path('products/<str:pk>', views.getProduct, name="product"),
+    # SIMPLEJWT Token URLs
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
