2c50e1387e301b44da7e2fec42a8b5c939703478
munozvicente <vanm@outlook.cl>
2022-09-07 15:36:55 -0300

    Cart Items are cleared after Order was successfully placed

--- frontend/my-app/src/actions/orderActions.js
+++ frontend/my-app/src/actions/orderActions.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 import { CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, CREATE_ORDER_FAIL } from "../constants/orderConstants";
 
+import { CART_CLEAR_ITEMS } from "../constants/cartConstants";
+
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
       dispatch({ type: CREATE_ORDER_REQUEST })
@@ -27,6 +29,13 @@ export const createOrder = (order) => async (dispatch, getState) => {
         type: CREATE_ORDER_SUCCESS,
         payload: data,
       });
+
+      dispatch({
+        type: CART_CLEAR_ITEMS,
+        payload: data,
+      });
+
+      localStorage.removeItem('cartItems');
   
     } catch (error) {
       dispatch({
