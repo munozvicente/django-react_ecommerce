1c816c3a9a3d21fbd747615dc2c451fe7012129f
munozvicente <vanm@outlook.cl>
2022-09-07 23:12:40 -0300

    Orders are now displayed in profile page

--- frontend/my-app/src/actions/userActions.js
+++ frontend/my-app/src/actions/userActions.js
@@ -22,6 +22,8 @@ import {
   USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
+import { ORDER_LIST_MY_RESET, } from "../constants/orderConstants"
+
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST })
@@ -61,7 +63,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) =>{
   localStorage.removeItem('userInfo');
   dispatch({type: USER_LOGOUT});
-  dispatch({type: USER_DETAILS_RESET})
+  dispatch({type: USER_DETAILS_RESET});
+  dispatch({type: ORDER_LIST_MY_RESET});
 }
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/my-app/src/screens/ProfileScreen.js
+++ frontend/my-app/src/screens/ProfileScreen.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
 
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 import { getUserDetails, updateUserProfile } from "../actions/userActions";
-import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants"
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
+import { listMyOrders } from "../actions/orderActions";
 
 function ProfileScreen() {
   const [name, setName] = useState("");
@@ -26,16 +28,20 @@ function ProfileScreen() {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
-  const updateProfile = useSelector((state) => state.userUpdateProfile);
-  const { success } = updateUserProfile;
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
+
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy;
 
   useEffect(() => {
     if (!userInfo) {
       navigate("/login");
     } else {
       if (!user || !user.name || success) {
-        dispatch({type: USER_UPDATE_PROFILE_RESET})
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
         dispatch(getUserDetails("profile"));
+        dispatch(listMyOrders());
       } else {
         setName(user.name);
         setEmail(user.email);
@@ -50,7 +56,14 @@ function ProfileScreen() {
     if (password !== confirmPassword) {
       setMessage("Passwords do not match");
     } else {
-      dispatch(updateUserProfile({'id': user._id, 'name': name, 'email': email, 'password': password}));
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          name: name,
+          email: email,
+          password: password,
+        })
+      );
       setMessage("");
     }
   };
@@ -110,6 +123,43 @@ function ProfileScreen() {
 
       <Col md={9}>
         <h2>My Orders</h2>
+        {loadingOrders ? (
+          <Loader />
+        ) : errorOrders ? (
+          <Message variant="danger">{errorOrders}</Message>
+        ) : (
+          <Table striped responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Paid</th>
+                <th>Delivered</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>${order.totalPrice}</td>
+                  <td>{order.isPaid ? order.paidAt : (
+                    <i className="fas fa-times" style={{color: "red"}}></i>)}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className="btn btn-primary">Details</Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+
+          </Table>
+        )}
       </Col>
     </Row>
   );
